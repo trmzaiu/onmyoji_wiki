cb9f6f123be25cc3b3a2f18f0203aaae8a9ccc73
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-09-22 04:34:10 +0700

    .

--- onmyoji_wiki/pages/shikigami/[name].vue
+++ onmyoji_wiki/pages/shikigami/[name].vue
@@ -749,7 +749,7 @@ watch(
 );
 
 watch(activeTab, async (newTab) => {
-  if (newTab === "Gallery" && illustrations.value.length === 0) {
+  if (newTab === "gallery" && illustrations.value.length === 0) {
     await fetchIllustrations(shikigami.value?.id);
   }
 });
