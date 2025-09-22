536c8efc4a4b0b26efd9f24da50dace6f11a9e49
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-09-23 01:52:16 +0700

    .

--- onmyoji_wiki/pages/shikigami/[name].vue
+++ onmyoji_wiki/pages/shikigami/[name].vue
@@ -1649,11 +1649,11 @@ const addCKeywordListeners = () => {
 
                 <td class="py-1 text-black table-cell w-[100px]">
                   <div class="w-12 h-12 flex items-center justify-center mx-auto relative">
-                    <img :src="shikigami.id === 144 ? '/assets/Jade.webp' : `https://twdujdgoxkgbvdkstske.supabase.co/storage/v1/object/public/Shikigami/Shards/${route.params.name}_Shard.webp`" :alt="shikigami.name.jp" class="max-h-full max-w-full object-contain" />
+                    <img :src="shikigami.id === 144 ? '/assets/Jade.webp' : (shikigami.id === 71 ? '/assets/Black_Daruma.webp' : `https://twdujdgoxkgbvdkstske.supabase.co/storage/v1/object/public/Shikigami/Shards/${route.params.name}_Shard.webp`)" :alt="shikigami.name.jp" class="max-h-full max-w-full object-contain" />
                     <span class="absolute bottom-0 right-0 text-white font-bold" style="
                       text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
                         1px 1px 0 #000;
-                    ">{{ (shikigami.id >= 201 && shikigami.id <= 217) ? 2 : 10 }}</span>
+                    ">{{ (shikigami.id >= 201 && shikigami.id <= 217) ? 2 : (shikigami.id === 71 ? '' : 10) }}</span>
                   </div>
                 </td>
               </tr>
