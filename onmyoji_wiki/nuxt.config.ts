4343df4f108d71588283053f2be2c2e110744c60
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-09-24 04:35:05 +0700

    .

--- onmyoji_wiki/nuxt.config.ts
+++ onmyoji_wiki/nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     public: {
