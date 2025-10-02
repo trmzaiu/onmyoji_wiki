a7a4a3ede0ef52102e5a9e19f81f793398b9701b
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-10-02 09:24:47 +0700

    .

--- onmyoji_wiki/pages/shikigami/index.vue
+++ onmyoji_wiki/pages/shikigami/index.vue
@@ -219,7 +219,7 @@ onMounted(async () => {
               <td class="px-2 py-1 text-center w-[160px]" style="border-left: 1px solid #e0e0e0">
                 <a :href="`/shikigami/${shiki.name.jp[1].replace(/ /g, '_')}`"
                   ><img
-                    :src="`https://twdujdgoxkgbvdkstske.supabase.co/storage/v1/object/public/Shikigami/Shards/${shiki.name.jp[1].replace(/ /g, '_')}_Shard.webp`"
+                    :src="`/assets/shikigami/shards/${shiki.name.jp[1].replace(/ /g, '_')}_Shard.webp`"
                     :alt="shiki.name.jp[1]"
                     class="w-16 h-16 object-contain mx-auto"
                 /></a>
