cecc1c76dd2f93d37d7b498076d15cf192f49d9c
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-11-03 06:03:06 +0700

    .

--- onmyoji_wiki/pages/shikigami/[name].vue
+++ onmyoji_wiki/pages/shikigami/[name].vue
@@ -2030,7 +2030,7 @@ const addCKeywordListeners = () => {
                 
                 <td class="px-2 py-2 text-center table-cell w-[105px]" v-if="skin.name.en === 'Default' || skin.name.en === 'Evolution'">
                   <div class="w-24 h-24 overflow-hidden">
-                    <img :src="skin.name.en === 'Default' ? `/assets/shikigami/skinsInfo/${route.params.name}_SkinInfo0.webp` : `/assets/shikigami/skinsInfo/${route.params.name}_SkinInfo00.webp`" :alt="skin.name.en || skin.name.cn"
+                    <img :src="skin.name.en === 'Default' ? (index === 0 && shikigami.id >= 201 && shikigami.id <= 217) ? `/assets/shikigami/shards/${route.params.name}_Shard.webp` : `/assets/shikigami/skinsInfo/${route.params.name}_SkinInfo0.webp` : `/assets/shikigami/skinsInfo/${route.params.name}_SkinInfo00.webp`" :alt="skin.name.en || skin.name.cn"
                       class="w-full h-full object-contain" :class="(index === 0 && shikigami.id >= 201 && shikigami.id <= 217) ? 'scale-145' : ''" />
                   </div>
                 </td>
