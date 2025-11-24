b6c59ae738e7529bf882a0a1199a09fcff31be9f
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-11-24 17:02:26 +0700

    c

--- onmyoji_wiki/pages/shikigami/[name].vue
+++ onmyoji_wiki/pages/shikigami/[name].vue
@@ -72,7 +72,7 @@ const getATKImage = (atk) => {
 // SS: 3618 -> 4153, S: 2948 -> 3597, A: 2894 -> 2921, B: 2412 -> 2466, C: 2144 -> 2385, D: 1822 -> 1849
 const getATKEvoRank = (atk) => {
   if (atk >= 3618 && atk <= 4153) return "SS";
-  else if (atk >= 2948 && atk <= 3597) return "S";
+  else if (atk >= 2948 && atk <= 3602) return "S";
   else if (atk >= 2734 && atk <= 2921) return "A";
   else if (atk >= 2412 && atk <= 2492) return "B";
   else if (atk >= 2144 && atk <= 2385) return "C";
