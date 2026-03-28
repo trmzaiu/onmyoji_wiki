6efae763d79238d600228d94ba243355441efc55
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2026-03-29 02:32:48 +0700

    x

--- onmyoji_wiki/pages/shikigami/[name].vue
+++ onmyoji_wiki/pages/shikigami/[name].vue
@@ -2670,11 +2670,11 @@ const addCKeywordListeners = () => {
 
               <td v-else class="py-1 text-black table-cell w-[100px]">
                 <div class="w-12 h-12 flex items-center justify-center mx-auto relative">
-                  <img :src="shikigami.id === 144 ? '/assets/Jade.webp' : (shikigami.id === 71 || shikigami.id === 84 ? '/assets/Black_Daruma.webp' : `/assets/shikigami/shards/${route.params.name}_Shard.webp`)" :alt="shikigami.name.jp" class="max-h-full max-w-full object-contain" />
+                  <img :src="[144, 118].includes(shikigami.id) ? '/assets/Jade.webp' : ([71, 84].includes(shikigami.id) ? '/assets/Black_Daruma.webp' : `/assets/shikigami/shards/${route.params.name}_Shard.webp`)" :alt="shikigami.name.jp" class="max-h-full max-w-full object-contain" />
                   <span class="absolute bottom-0 right-0 text-white font-bold" style="
                     text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
                       1px 1px 0 #000;
-                  ">{{ (shikigami.id >= 201 && shikigami.id <= 214) ? 2 : (shikigami.id === 71 || shikigami.id === 84 ? '' : 10) }}</span>
+                  ">{{ (shikigami.id >= 201 && shikigami.id <= 214) ? 2 : ([71, 84].includes(shikigami.id) ? '' : 10) }}</span>
                 </div>
               </td>
             </tr>
@@ -2689,7 +2689,7 @@ const addCKeywordListeners = () => {
 
               <td class="py-1 text-black table-cell w-[100px]">
                 <div class="w-12 h-12 flex items-center justify-center mx-auto relative">
-                  <img :src="shikigami.id !== 144 ? '/assets/Jade.webp' : `/assets/shikigami/shards/${route.params.name}_Shard.webp`" alt="Jade" class="max-h-full max-w-full object-contain" />
+                  <img :src="![144, 118].includes(shikigami.id) ? '/assets/Jade.webp' : `/assets/shikigami/shards/${route.params.name}_Shard.webp`" alt="Jade" class="max-h-full max-w-full object-contain" />
                   <span class="absolute bottom-0 right-0 text-white font-bold" style="
                     text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
                       1px 1px 0 #000;
